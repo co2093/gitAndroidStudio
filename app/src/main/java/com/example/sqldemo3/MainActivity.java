f4af0ffd506504b4c2167b51ba24e9233721ff12
co2093 <rammfilth6>
2021-01-31 22:43:54 -0600

    Test 3
    Signed-off-by: co2093 <rammfilth6>

--- app/src/main/java/com/example/sqldemo3/MainActivity.java
+++ app/src/main/java/com/example/sqldemo3/MainActivity.java
@@ -37,15 +37,21 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
+                CustomerModel customerModel;
+
                 try {
-                    CustomerModel customerModel = new CustomerModel(-1, et_name.getText().toString(), Integer.parseInt(et_age.getText().toString()),sw_activeCustomer.isChecked());
+                    customerModel = new CustomerModel(-1, et_name.getText().toString(), Integer.parseInt(et_age.getText().toString()),sw_activeCustomer.isChecked());
                     Toast.makeText(MainActivity.this, customerModel.toString(), Toast.LENGTH_LONG).show();
                 }catch (Exception e){
                     Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_LONG).show();
-
+                    customerModel = new CustomerModel(-1, "error", 0, false);
                 }
 
+                DataBaseHelper dataBaseHelper = new DataBaseHelper(MainActivity.this);
 
+                boolean success = dataBaseHelper.addOne(customerModel);
+
+                Toast.makeText(MainActivity.this, "Success="+success, Toast.LENGTH_SHORT).show();
             }
         });
 
